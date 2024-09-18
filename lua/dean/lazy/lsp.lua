a066428dc74aa081bd48dbfe813576b4c07a9556
deanblunting <abhinavdangi.in@gmail.com>
2024-09-18 16:38:35 +0530

    added lsp for clang

--- lua/dean/lazy/lsp.lua
+++ lua/dean/lazy/lsp.lua
@@ -28,6 +28,7 @@ return {
             ensure_installed = {
                 "lua_ls",
                 "pylsp",
+                "clangd",
             },
             handlers = {
                 function(server_name)
